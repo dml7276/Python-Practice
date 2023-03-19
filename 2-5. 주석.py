7ec93daa84fc97badd0a6d10f48c19161dc76562
Plusmate <dml7276@gmail.com>
2023-03-19 15:48:44 +0900

    P2

---
+++ 2-5. 주석.py
@@ -0,0 +1,15 @@
+'''
+이렇게
+하면
+여러문장이
+주석처리
+됩니다.
+'''
+
+# 여러 문장을 일괄 주석 처리를 하고 싶으면 ctrl + /, 다시 해제도 ctrl + /
+
+# print("우리집에는 " + animal + "가 있는데 이름이 " + name + "이에요.")
+# hobby = "공놀이"
+# # print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요.")
+# print(name, "는", age, "살이며,", hobby, "을 아주 좋아해요.")
+# print(name + "는 어른일까요? " + str(is_adult))
